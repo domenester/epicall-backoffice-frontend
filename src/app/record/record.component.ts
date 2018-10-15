933133105793bf561feae60c73846974aabd4805
domenester <diogodomene@gmail.com>
2018-10-15 13:41:30 -0300

    Adding records filter

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
-import { AlertService, RecordService } from '../services';
+import { AlertService, RecordService, UserService } from '../services';
 import { PlayerViewComponent } from './player-view/player-view.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbDateStruct, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import * as moment from 'moment';
 import { tableSettings } from './config/table-config';
+import { NgbDateBRParserFormatter, unixValue } from '../utils/date';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-record',
   templateUrl: './record.component.html',
   styleUrls: ['./record.component.scss'],
-  entryComponents: [ PlayerViewComponent ]
+  entryComponents: [ PlayerViewComponent ],
+  providers: [
+    { provide: NgbDateParserFormatter, useClass: NgbDateBRParserFormatter}
+  ]
 })
 export class RecordComponent implements OnInit {
 
@@ -18,14 +23,19 @@ export class RecordComponent implements OnInit {
   loading = true;
   tableSettings: any;
   source: LocalDataSource;
+  usersDropDown: any = [];
+  dateStartFilter: NgbDateStruct;
+  dateEndFilter: NgbDateStruct;
+  form: FormGroup;
 
   constructor(
+    private formBuilder: FormBuilder,
     private alertService: AlertService,
     private recordService: RecordService,
+    private userService: UserService,
     private modalService: NgbModal) {
       this.recordService.list().subscribe(
         records => {
-          console.log('records', records);
           this.records = records;
           this.source = new LocalDataSource(records);
           this.loading = false;
@@ -33,8 +43,29 @@ export class RecordComponent implements OnInit {
         error => {
           this.alertService.error(error.error.message);
           this.loading = false;
-        });
+        }
+      );
+
+      this.userService.list().subscribe(
+        users => {
+          this.usersDropDown = users.map( user => {
+            return {
+              id: user.id,
+              name: `${user.first_name} ${user.last_name}`
+             };
+          });
+        }
+      );
+
+      this.form = this.formBuilder.group({
+        user: [''],
+        start: [''],
+        end: [''],
+        ext: ['']
+      });
     }
+
+  get f() { return this.form.controls; }
 
   ngOnInit() {
     this.tableSettings = tableSettings();
@@ -49,5 +80,21 @@ export class RecordComponent implements OnInit {
       { field: 'date', search: query  },
       { field: 'duration', search: query  }
     ], false);
+  }
+
+  onSubmit() {
+    const userId = this.f.user.value;
+    let params = '?';
+    if (this.f.user.value) { params += `&userId=${this.f.user.value}`; }
+    if (this.f.start.value) { params += `&start=${unixValue(this.f.start.value)}`; }
+    if (this.f.end.value) { params += `&end=${unixValue(this.f.end.value)}`; }
+    if (this.f.ext.value) { params += `&ext=${this.f.ext.value}`; }
+    this.recordService.list(params).subscribe(
+      records => {
+        this.records = records;
+        this.form.reset();
+      },
+      error => this.alertService.error(error.error.message),
+    );
   }
 }
---
+++ src/app/utils/date.ts
@@ -0,0 +1,36 @@
+import {NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Injectable } from '@angular/core';
+import { isNumber, padNumber, toInteger } from './numbers';
+import * as moment from 'moment';
+
+@Injectable()
+export class NgbDateBRParserFormatter extends NgbDateParserFormatter {
+  parse(value: string): NgbDateStruct {
+    if (value) {
+        const dateParts = value.trim().split('/');
+        if (dateParts.length === 1 && isNumber(dateParts[0])) {
+            return {year: toInteger(dateParts[0]), month: null, day: null};
+        } else if (dateParts.length === 2 && isNumber(dateParts[0]) && isNumber(dateParts[1])) {
+            return {year: toInteger(dateParts[1]), month: toInteger(dateParts[0]), day: null};
+        } else if (dateParts.length === 3 && isNumber(dateParts[0]) && isNumber(dateParts[1]) && isNumber(dateParts[2])) {
+            return {year: toInteger(dateParts[2]), month: toInteger(dateParts[1]), day: toInteger(dateParts[0])};
+        }
+    }
+    return null;
+}
+  format(date: NgbDateStruct): string {
+    let stringDate = '';
+    if (date) {
+        stringDate += isNumber(date.day) ? padNumber(date.day) + '/' : '';
+        stringDate += isNumber(date.month) ? padNumber(date.month) + '/' : '';
+        stringDate += date.year;
+    }
+    return stringDate;
+  }
+}
+
+export const unixValue = (d: NgbDateStruct) => {
+  return moment(
+    moment(`${d.year}/${d.month}/${d.day}`)
+  ).unix();
+};
